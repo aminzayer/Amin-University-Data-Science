aadbef0b6898dc761361b5144273399a321ea173
Amin Zayeromali <amin.zayeromali@gmail.com>
2022-02-07 18:16:37 +0330

    New Update & add ...

--- Linear-Programing-Optimizing/Problem-10.py
+++ Linear-Programing-Optimizing/Problem-10.py
@@ -1,7 +1,7 @@
-# Maximize:  Z = 3x + 5y
+# Maximize:  Z = 3x - 2y
 # Subject to the constraints:
-# x + 2y >= 4
-# 2x + y <= 8
+# x + 2y <= 6
+# 2x - y <= 3
 # x, y >= 0
 
 # import the library pulp as p
@@ -15,11 +15,11 @@ x = p.LpVariable("x", lowBound=0)  # Create a variable x >= 0
 y = p.LpVariable("y", lowBound=0)  # Create a variable y >= 0
 
 # Objective Function
-Lp_prob += 3 * x + 5 * y
+Lp_prob += 3 * x - 2 * y
 
 # Constraints:
-Lp_prob += x + 2 * y >= 4
-Lp_prob += 2 * x + y <= 8
+Lp_prob += x + 2 * y <= 6
+Lp_prob += 2 * x - y <= 3
 
 # Display the problem
 print(Lp_prob)
